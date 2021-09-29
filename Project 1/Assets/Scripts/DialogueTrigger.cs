7f1bb9671f53aeb1ff2cfa80acb06e47c2f14260
Nerota02 <91302583+Nerota02@users.noreply.github.com>
2021-09-29 05:11:48 -0400

    adding more to dialog

--- Project 1/Assets/Scripts/DialogueTrigger.cs
+++ Project 1/Assets/Scripts/DialogueTrigger.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-
+    public bool playerInRange;
     public Dialogue dialogue;
+    public DialogueManager dialogueManager;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            TriggerDialogue();
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        playerInRange = false;
+        if (dialogueManager.animator.GetBool("isOpen"))
+        {
+            dialogueManager.EndDialog();
+        }
+    }
 
     public void TriggerDialogue()
     {
