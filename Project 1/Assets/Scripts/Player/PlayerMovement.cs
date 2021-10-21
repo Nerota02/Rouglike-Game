6000121456543c54c8776fc5a7336ca28c76e37f
Nerota02 <91302583+Nerota02@users.noreply.github.com>
2021-10-21 00:14:37 -0400

    adding to inventory system

--- Project 1/Assets/Scripts/Player/PlayerMovement.cs
+++ Project 1/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Level level;
     public InventoryObject inventory;
     public float moveSpeed = 5f;
     public Transform movePoint;
@@ -13,10 +15,22 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         movePoint.parent = null;
+        level = new Level(1, OnLevelUp);
+    }
+
+    public void OnLevelUp()
+    {
+        print("I leveled up!");
     }
 
     void Update()
     {
+        //Test Exp 
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            level.AddExp(100);
+        }
+
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
         if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
@@ -41,17 +55,33 @@ public class PlayerMovement : MonoBehaviour
 
         }
 
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            inventory.Save();
+            Debug.Log("Game Saved");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            inventory.Load();
+            Debug.Log("Game Loaded");
+        }
+
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        var item = collision.GetComponent<Item>();
+        var item = collision.GetComponent<GroundItem>();
         if (item)
         {
-            inventory.AddItem(item.item, 1);
+            inventory.AddItem(new Item(item.item), 1);
             Destroy(collision.gameObject);
         }
+    }
+
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Items.Clear();
     }
 
 
